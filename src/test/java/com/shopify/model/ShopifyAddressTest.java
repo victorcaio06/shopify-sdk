694c96de63f0a5bd5f463577fced808df9f003b6
Eduardo de Oliveira Sousa <84216150+eduardo-559@users.noreply.github.com>
2023-11-13 03:27:42 -0300

    Update ShopifyAddressTest.java

--- src/test/java/com/shopify/model/ShopifyAddressTest.java
+++ src/test/java/com/shopify/model/ShopifyAddressTest.java
@@ -6,14 +6,13 @@ import java.math.BigDecimal;
 
 import org.junit.Test;
 
-public class ShopifyAddressTest {
+public class ShopifyAddressUpdateRequestTest {
 
 	private static final String SOME_PROVINCE_CODE = "PA";
 	private static final String SOME_PROVINCE = "Pennsylvania";
 	private static final String SOME_PHONE = "3874928734234";
 	private static final BigDecimal SOME_LONGITUDE = new BigDecimal(485.8323);
 	private static final BigDecimal SOME_LATITUDE = new BigDecimal(41.489);
-	private static final String SOME_NAME = "Ryan Kazokas";
 	private static final String SOME_LAST_NAME = "Kazokas";
 	private static final String SOME_FIRST_NAME = "Ryan";
 	private static final String SOME_COUNTRY_CODE = "US";
@@ -24,38 +23,46 @@ public class ShopifyAddressTest {
 	private static final String SOME_ADDRESS_1 = "224 Wyoming Ave";
 
 	@Test
-	public void givenSomeValuesWhenBuildingShopifyAddressThenExpectCorrectValues() {
-
-		final ShopifyAddress shopifyAddress = new ShopifyAddress();
-		shopifyAddress.setAddress1(SOME_ADDRESS_1);
-		shopifyAddress.setAddress2(SOME_ADDRESS_2);
-		shopifyAddress.setCity(SOME_CITY);
-		shopifyAddress.setCompany(SOME_COMPANY);
-		shopifyAddress.setCountry(SOME_COUNTRY);
-		shopifyAddress.setCountryCode(SOME_COUNTRY_CODE);
-		shopifyAddress.setFirstName(SOME_FIRST_NAME);
-		shopifyAddress.setLastname(SOME_LAST_NAME);
-		shopifyAddress.setName(SOME_NAME);
-		shopifyAddress.setLatitude(SOME_LATITUDE);
-		shopifyAddress.setLongitude(SOME_LONGITUDE);
-		shopifyAddress.setPhone(SOME_PHONE);
-		shopifyAddress.setProvince(SOME_PROVINCE);
-		shopifyAddress.setProvinceCode(SOME_PROVINCE_CODE);
-
-		assertEquals(SOME_ADDRESS_1, shopifyAddress.getAddress1());
-		assertEquals(SOME_ADDRESS_2, shopifyAddress.getAddress2());
-		assertEquals(SOME_CITY, shopifyAddress.getCity());
-		assertEquals(SOME_COMPANY, shopifyAddress.getCompany());
-		assertEquals(SOME_COUNTRY, shopifyAddress.getCountry());
-		assertEquals(SOME_COUNTRY_CODE, shopifyAddress.getCountryCode());
-		assertEquals(SOME_FIRST_NAME, shopifyAddress.getFirstName());
-		assertEquals(SOME_LAST_NAME, shopifyAddress.getLastname());
-		assertEquals(SOME_LATITUDE, shopifyAddress.getLatitude());
-		assertEquals(SOME_LONGITUDE, shopifyAddress.getLongitude());
-		assertEquals(SOME_PHONE, shopifyAddress.getPhone());
-		assertEquals(SOME_PROVINCE, shopifyAddress.getProvince());
-		assertEquals(SOME_PROVINCE_CODE, shopifyAddress.getProvinceCode());
-
+	public void givenSomeValuesWhenBuildingShopifyAddressUpdateRequestThenExpectCorrectValues() {
+		// Arrange
+		final ShopifyAddressUpdateRequest shopifyAddressUpdateRequest = buildShopifyAddressUpdateRequest();
+	
+		// Act and Assert
+		assertShopifyAddressUpdateRequestValues(shopifyAddressUpdateRequest);
 	}
-
+	
+	private ShopifyAddressUpdateRequest buildShopifyAddressUpdateRequest() {
+		final ShopifyAddressUpdateRequest shopifyAddressUpdateRequest = new ShopifyAddressUpdateRequest();
+		shopifyAddressUpdateRequest.setAddress1(SOME_ADDRESS_1);
+		shopifyAddressUpdateRequest.setAddress2(SOME_ADDRESS_2);
+		shopifyAddressUpdateRequest.setCity(SOME_CITY);
+		shopifyAddressUpdateRequest.setCompany(SOME_COMPANY);
+		shopifyAddressUpdateRequest.setCountry(SOME_COUNTRY);
+		shopifyAddressUpdateRequest.setCountryCode(SOME_COUNTRY_CODE);
+		shopifyAddressUpdateRequest.setFirstName(SOME_FIRST_NAME);
+		shopifyAddressUpdateRequest.setLastname(SOME_LAST_NAME);
+		shopifyAddressUpdateRequest.setLatitude(SOME_LATITUDE);
+		shopifyAddressUpdateRequest.setLongitude(SOME_LONGITUDE);
+		shopifyAddressUpdateRequest.setPhone(SOME_PHONE);
+		shopifyAddressUpdateRequest.setProvince(SOME_PROVINCE);
+		shopifyAddressUpdateRequest.setProvinceCode(SOME_PROVINCE_CODE);
+		return shopifyAddressUpdateRequest;
+	}
+	
+	private void assertShopifyAddressUpdateRequestValues(ShopifyAddressUpdateRequest request) {
+		assertEquals(SOME_ADDRESS_1, request.getAddress1());
+		assertEquals(SOME_ADDRESS_2, request.getAddress2());
+		assertEquals(SOME_CITY, request.getCity());
+		assertEquals(SOME_COMPANY, request.getCompany());
+		assertEquals(SOME_COUNTRY, request.getCountry());
+		assertEquals(SOME_COUNTRY_CODE, request.getCountryCode());
+		assertEquals(SOME_FIRST_NAME, request.getFirstName());
+		assertEquals(SOME_LAST_NAME, request.getLastname());
+		assertEquals(SOME_LATITUDE, request.getLatitude());
+		assertEquals(SOME_LONGITUDE, request.getLongitude());
+		assertEquals(SOME_PHONE, request.getPhone());
+		assertEquals(SOME_PROVINCE, request.getProvince());
+		assertEquals(SOME_PROVINCE_CODE, request.getProvinceCode());
+	}
+	
 }
